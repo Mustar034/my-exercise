156ca1b61a31248311cb2eb8fb4a7655b65d69e9
Mustar034 <3192705696@qq.com>
2026-06-03 15:00:08 +0800

    二次提交

--- ex01/Helloworld.c
+++ ex01/Helloworld.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main() {
+    char inputStr[100];
     printf("Hello World!\n");
+    printf("Please enter a string: ");
+    scanf("%s",inputStr);
+    printf("You entered: %s\n", inputStr);
     return 0;
 }
